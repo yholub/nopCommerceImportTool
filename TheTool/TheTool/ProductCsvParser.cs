4abb294d8434f33260c94536f245713c72737121
Yaroslav Holub <yaroslav.holub@sigmatechnology.se>
2019-07-12 10:05:07 +0300

    add basic csv Reader

---
+++ TheTool/TheTool/ProductCsvParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TheTool
+{
+    class ProductCsvParser
+    {
+        public IEnumerable<Product> Parse(string csvFilePath)
+        {
+            List<Product> res = new List<Product>();
+
+            var lines = File.ReadAllLines(csvFilePath)
+                .Select(a => a.Split(';'))
+                .ToArray();
+
+            foreach (var row in lines)
+            {
+                var p = new Product
+                {
+                    Name = ParseName(row[0]),
+                    Price = parsePrice(row[1]),
+                    SKU = ParseSku(row[2]),
+                    Attributes = new Attributes
+                    {
+                        AgeFrom = ParseAgeFrom(row[3]),
+                        ...
+                    }
+                }
+            }
+        } 
+    }
+}
